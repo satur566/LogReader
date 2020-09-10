979cb50aba334198bb243534749108c91a34f729
Alexandr Maximov <satur566@gmail.com>
2020-09-10 16:24:39 +0300

    First commit.

---
+++ LogReader/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogReader
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] fullLog = File.ReadAllLines(args[0]);
+            string reqDate = Console.ReadLine();
+            for (int i = 0; i < fullLog.Length; i++)
+            {
+                if (fullLog[i].Contains(reqDate))
+                {
+                    Console.WriteLine(fullLog[i]);
+                } 
+            }
+        }
+    }
+}
